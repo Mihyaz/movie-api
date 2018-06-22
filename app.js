00a6f4cd797d91e6ba7995ed81e23388907ab248
Mihyaz <40426286+Mihyaz@users.noreply.github.com>
2018-06-22 10:48:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const movie = require('./routes/movie');
+const director = require('./routes/director');
+
+const app = express();
+const db = require('./helper/db.js')();
+//config
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
+//middleware
+const verifyToken = require('./middleware/verify-token');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/api/movie', movie);
+app.use('/api/director', director);
+app.use('/api', verifyToken);
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) =>{
+  next(createError(404));
+});
+//db connection
+
+// error handler
+app.use((err, req, res, next)=> {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({error: {message: err.message, code: err.code}});
+});
+
+module.exports = app;
